04d97bed1b8ca1a46f032ef575e99fd2e4d052a2
CCIA <yzenteno@cciarequipa.org>
2022-09-08 16:07:54 -0500

    adicion de barra rosa y hover a link de menu

--- arbitrajeweb/js/index.js
+++ arbitrajeweb/js/index.js
@@ -5,12 +5,15 @@ $(window).scroll(function(event) {
     if(scrollTop >= 100){
       document.getElementById('nav-sticky').style.backgroundColor='#f5f5f5';
       document.getElementById('navbar-brand-sticky').style.height = '58'+'px';
-      //document.getElementById('navbar-sticky').classList.remove('my-3');
+      document.getElementById('nav-top').classList.remove('d-flex');
+      document.getElementById('nav-top').classList.add('d-none');
     }
   
    if(scrollTop < 100){
       document.getElementById('nav-sticky').style.backgroundColor='#fff';
       document.getElementById('navbar-brand-sticky').style.height = '68'+'px';
+      document.getElementById('nav-top').classList.add('d-flex');
+      document.getElementById('nav-top').classList.remove('d-none');
     }
   
 });
