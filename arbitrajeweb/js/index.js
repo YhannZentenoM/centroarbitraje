ec3de5111b78fca56da70b6ae0da25360e6ec9c0
CCIA <yzenteno@cciarequipa.org>
2022-09-08 19:43:25 -0500

    correcion distancia slider media query 1440px

--- arbitrajeweb/js/index.js
+++ arbitrajeweb/js/index.js
@@ -5,15 +5,20 @@ $(window).scroll(function(event) {
     if(scrollTop >= 100){
       document.getElementById('nav-sticky').style.backgroundColor='#f5f5f5';
       document.getElementById('navbar-brand-sticky').style.height = '58'+'px';
-      document.getElementById('nav-top').classList.remove('d-flex');
-      document.getElementById('nav-top').classList.add('d-none');
+      //document.getElementById('nav-top').classList.remove('d-flex');
+      //document.getElementById('nav-top').classList.add('d-none');
+      //document.getElementById('nav-top').classList.remove('d-flex');
+      document.getElementById('nav-top').style.visibility='hidden';
+      document.getElementById('nav-top').style.height='0';
     }
   
    if(scrollTop < 100){
       document.getElementById('nav-sticky').style.backgroundColor='#fff';
       document.getElementById('navbar-brand-sticky').style.height = '68'+'px';
-      document.getElementById('nav-top').classList.add('d-flex');
-      document.getElementById('nav-top').classList.remove('d-none');
+      //document.getElementById('nav-top').classList.add('d-flex');
+      //document.getElementById('nav-top').classList.remove('d-none');
+      document.getElementById('nav-top').style.visibility='visible';
+      document.getElementById('nav-top').style.height='23px';
     }
   
 });
