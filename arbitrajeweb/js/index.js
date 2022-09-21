99cd75f65ee3976d00a681929ef8c0c65c2a6442
CCIA <yzenteno@cciarequipa.org>
2022-09-21 11:42:16 -0500

    pagina index

--- arbitrajeweb/js/index.js
+++ arbitrajeweb/js/index.js
@@ -8,8 +8,8 @@ $(window).scroll(function(event) {
       //document.getElementById('nav-top').classList.remove('d-flex');
       //document.getElementById('nav-top').classList.add('d-none');
       //document.getElementById('nav-top').classList.remove('d-flex');
-      document.getElementById('nav-top').style.visibility='hidden';
-      document.getElementById('nav-top').style.height='0';
+      // document.getElementById('nav-top').style.visibility='hidden';
+      // document.getElementById('nav-top').style.height='0';
     }
   
    if(scrollTop < 100){
@@ -17,8 +17,25 @@ $(window).scroll(function(event) {
       document.getElementById('navbar-brand-sticky').style.height = '68'+'px';
       //document.getElementById('nav-top').classList.add('d-flex');
       //document.getElementById('nav-top').classList.remove('d-none');
-      document.getElementById('nav-top').style.visibility='visible';
-      document.getElementById('nav-top').style.height='23px';
+      // document.getElementById('nav-top').style.visibility='visible';
+      // document.getElementById('nav-top').style.height='23px';
     }
   
+});
+
+$(document).ready(function() {
+  $('#slider').lightSlider({
+      easing: 'easeOutQuad',
+      mode: "fade",
+      auto:true,
+      loop:true,
+      item:1,
+      speed:600,
+      pause: 5000,
+      controls: true,
+      prevHtml: '',
+      nextHtml: '',
+      slideEndAnimation: true,
+      pauseOnHover: true,
+  });
 });
